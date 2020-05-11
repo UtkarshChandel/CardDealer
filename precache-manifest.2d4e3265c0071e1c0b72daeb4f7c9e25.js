f36a6800cae8eec81702e6e4509ab45aef04971d
UtkarshChandel <personal.utkarsh24@gmail.com>
2020-05-11 14:35:01 +0530

    Updates

---
+++ precache-manifest.2d4e3265c0071e1c0b72daeb4f7c9e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2d61612ae3fc06db121544a078d900",
+    "url": "/CardDealer/index.html"
+  },
+  {
+    "revision": "7bf941dd5dd6468aacab",
+    "url": "/CardDealer/static/css/main.2d23a6d4.chunk.css"
+  },
+  {
+    "revision": "d51be6aaf3f9cddc550f",
+    "url": "/CardDealer/static/js/2.e1dedeaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CardDealer/static/js/2.e1dedeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf941dd5dd6468aacab",
+    "url": "/CardDealer/static/js/main.593593e5.chunk.js"
+  },
+  {
+    "revision": "42f056d370f8dff519b4",
+    "url": "/CardDealer/static/js/runtime-main.72b7a2c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CardDealer/static/media/logo.5d5d9eef.svg"
+  }
+]);
